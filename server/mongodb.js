b49375d2c835e19806e26e386989d07b95117644
Saket <saketbhatt21@gmail.com>
2020-01-16 16:50:29 +0530

    mongodb packages installed and db initialized

---
+++ server/mongodb.js
@@ -0,0 +1,20 @@
+const mongodb = require('mongodb')
+
+
+const MongoClient = mongodb.MongoClient
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'manager'
+
+
+
+MongoClient.connect(connectionURL,{useUnifiedTopology: true},(error,client) =>{
+	if(error){
+		return console.log("Unable to connect to database")
+	}
+	console.log("connected!")
+	const db = client.db(databaseName)
+
+	/*db.collection('organization').insertOne({
+
+	})*/
+})
