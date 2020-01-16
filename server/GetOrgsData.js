e434cfd31ceb6153a93c1648230c8c6d85167401
Saket <saketbhatt21@gmail.com>
2020-01-17 05:05:30 +0530

    mongo db setup

---
+++ server/GetOrgsData.js
@@ -0,0 +1,54 @@
+ 
+const organizationArray =  [
+    {
+        name: 'Organization A',
+        description: 'Lorem olor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.',
+        established: 2005,
+        location: 'London',
+        website: 'www.org.com',
+        donation: 'www.org.com/donate'
+    },
+    {
+        name: 'Organization B',
+        description: 'Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.',
+        established: 2015,
+        location: 'India',
+        website: 'www.org.com',
+        donation: 'www.org.com/donate'
+    },
+    {
+        name: 'Organization c',
+        description: 'Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.',
+        established: 2017,
+        location: 'India',
+        website: 'www.org.com',
+        donation: 'www.org.com/donate'
+    },
+    {
+        name: 'Organization c',
+        description: 'Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.',
+        established: 2017,
+        location: 'India',
+        website: 'www.org.com',
+        donation: 'www.org.com/donate'
+    },
+    {
+        name: 'Organization c',
+        description: 'Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.',
+        established: 2017,
+        location: 'India',
+        website: 'www.org.com',
+        donation: 'www.org.com/donate'
+    },
+    {
+        name: 'Organization c',
+        description: 'Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.Lorem ipsum dolor sit amet, diam consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.',
+        established: 2017,
+        location: 'India',
+        website: 'www.org.com',
+        donation: 'www.org.com/donate'
+    }
+
+]
+
+module.exports.arr = organizationArray
--- server/mongodb.js
+++ server/mongodb.js
@@ -1,10 +1,10 @@
 const mongodb = require('mongodb')
-
+const organizationArray = require('./GetOrgsData')
+console.log(organizationArray.arr)
 
 const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'manager'
-
 
 
 MongoClient.connect(connectionURL,{useUnifiedTopology: true},(error,client) =>{
@@ -12,9 +12,21 @@ MongoClient.connect(connectionURL,{useUnifiedTopology: true},(error,client) =>{
 		return console.log("Unable to connect to database")
 	}
 	console.log("connected!")
+
 	const db = client.db(databaseName)
 
-	/*db.collection('organization').insertOne({
+	
+	db.collection('organization').insertMany(
+		organizationArray.arr, 
+		(error,result) =>{
+			if(error){
+				return console.log(error)
+			}
+			console.log("Successfully inserted " + result.insertedCount + "items")
+		}
+		)
+		
+	
 
-	})*/
-})
+
+	})
