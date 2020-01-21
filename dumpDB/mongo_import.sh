6a719f3473f24ce6f25923d2aee667bbb214a93c
harsh <harshjaiswal253@gmail.com>
2020-01-21 14:25:11 +0530

    Added dump Data

---
+++ dumpDB/mongo_import.sh
@@ -0,0 +1,3 @@
+mongoimport -d HackingHomeless -c organizations exp_HackingHomeless_organizations.json &
+mongoimport -d HackingHomeless -c news exp_HackingHomeless_news.json &
+mongoimport -d HackingHomeless -c ideas exp_HackingHomeless_ideas.json
